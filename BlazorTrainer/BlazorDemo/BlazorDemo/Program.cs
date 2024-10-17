087170bba3e6d7687e83eb1a69ff0acf4dcd1897
John Muraski <john.t.muraski@outlook.com>
2024-10-17 00:44:43 -0500

    Routing training complete

--- BlazorTrainer/BlazorDemo/BlazorDemo/Program.cs
+++ BlazorTrainer/BlazorDemo/BlazorDemo/Program.cs
@@ -1,10 +1,15 @@
 using BlazorDemo.Client.Pages;
 using BlazorDemo.Components;
 
+// Program.cs is the entry point for the application. It is where the application is configured and started. It is the first file that is run when the application is started
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
+// This is the spot where you will add any dependency injections that you need
+// for your application. As well as any services.
+
+// Add services to the container.
+builder.Services.AddRazorComponents() // This is what adds Blazor to the application
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
@@ -24,12 +29,13 @@ else
 
 app.UseHttpsRedirection();
 
-app.UseStaticFiles();
-app.UseAntiforgery();
+app.UseStaticFiles(); // This is where files, like images or CSS files can be injected here
+app.UseAntiforgery(); // Helps to tell if a user submitted a form from your site or a bot
 
-app.MapRazorComponents<App>()
+
+app.MapRazorComponents<App>() //This is where the Blazor components are mapped to the application, starting with the App component/page
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(BlazorDemo.Client._Imports).Assembly);
 
-app.Run();
+app.Run(); //Start and run the applicationj
